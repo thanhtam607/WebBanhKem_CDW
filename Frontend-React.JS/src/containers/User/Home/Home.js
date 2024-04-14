d9fa4c160ac655400168c242bee64089961f8edf
Tin nguyen <20130436@st.hcmuaf.edu.vn>
2024-04-14 13:01:42 +0700

    ui home

--- Frontend-React.JS/src/containers/User/Home/Home.js
+++ Frontend-React.JS/src/containers/User/Home/Home.js
@@ -511,6 +511,235 @@ class HOME extends Component {
             </div>
           </div>
         </div>
+
+
+             {/* Banner Section  */}
+        <div className="container-fluid banner bg-secondary my-5">
+          <div className="container py-5">
+            <div className="row g-4 align-items-center">
+              <div className="col-lg-6">
+                <div className="py-4">
+                  <h1 className="display-3 text-white">Thăng hoa trong vị ngọt</h1>
+                  <p className="fw-normal display-3 text-dark mb-4">cùng chúng tôi</p>
+                  <p className="mb-4 text-dark">Khám phá bộ sưu tập bánh kem đa dạng và phong phú của chúng tôi để làm cho mọi dịp đặc biệt hơn.</p>
+                  <a href="#" className="banner-btn btn border-2 border-white rounded-pill text-dark py-3 px-5">Mua ngay</a>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="position-relative">
+                  <img src="img/baner-1.png" className="img-fluid w-100 rounded" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Banner Section End */}
+
+         {/* Bestsaler Product Start */}
+      <div className="container-fluid py-5">
+        <div className="container py-5">
+          <div className="text-center mx-auto mb-5" style={{maxWidth: '700px'}}>
+            <h1 className="display-4">Sản phẩm bán chạy</h1>
+          </div>
+          <div className="row g-4">
+            <div className="col-lg-6 col-xl-4">
+              <div className="p-4 rounded bg-light">
+                <div className="row align-items-center">
+                  <div className="col-6">
+                    <img src="img/best-product-1.jpg" className="img-fluid rounded-circle w-100" alt="" />
+                  </div>
+                  <div className="col-6">
+                    <a href="#" className="h5">Organic Tomato</a>
+                    <div className="d-flex my-3">
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star" />
+                    </div>
+                    <h4 className="mb-3">3.12 $</h4>
+                    <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary" /> Add to cart</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-xl-4">
+              <div className="p-4 rounded bg-light">
+                <div className="row align-items-center">
+                  <div className="col-6">
+                    <img src="img/best-product-2.jpg" className="img-fluid rounded-circle w-100" alt="" />
+                  </div>
+                  <div className="col-6">
+                    <a href="#" className="h5">Organic Tomato</a>
+                    <div className="d-flex my-3">
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star" />
+                    </div>
+                    <h4 className="mb-3">3.12 $</h4>
+                    <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary" /> Add to cart</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-xl-4">
+              <div className="p-4 rounded bg-light">
+                <div className="row align-items-center">
+                  <div className="col-6">
+                    <img src="img/best-product-3.jpg" className="img-fluid rounded-circle w-100" alt="" />
+                  </div>
+                  <div className="col-6">
+                    <a href="#" className="h5">Organic Tomato</a>
+                    <div className="d-flex my-3">
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star" />
+                    </div>
+                    <h4 className="mb-3">3.12 $</h4>
+                    <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary" /> Add to cart</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-xl-4">
+              <div className="p-4 rounded bg-light">
+                <div className="row align-items-center">
+                  <div className="col-6">
+                    <img src="img/best-product-4.jpg" className="img-fluid rounded-circle w-100" alt="" />
+                  </div>
+                  <div className="col-6">
+                    <a href="#" className="h5">Organic Tomato</a>
+                    <div className="d-flex my-3">
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star" />
+                    </div>
+                    <h4 className="mb-3">3.12 $</h4>
+                    <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary" /> Add to cart</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-xl-4">
+              <div className="p-4 rounded bg-light">
+                <div className="row align-items-center">
+                  <div className="col-6">
+                    <img src="img/best-product-5.jpg" className="img-fluid rounded-circle w-100" alt="" />
+                  </div>
+                  <div className="col-6">
+                    <a href="#" className="h5">Organic Tomato</a>
+                    <div className="d-flex my-3">
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star" />
+                    </div>
+                    <h4 className="mb-3">3.12 $</h4>
+                    <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary" /> Add to cart</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-xl-4">
+              <div className="p-4 rounded bg-light">
+                <div className="row align-items-center">
+                  <div className="col-6">
+                    <img src="img/best-product-6.jpg" className="img-fluid rounded-circle w-100" alt="" />
+                  </div>
+                  <div className="col-6">
+                    <a href="#" className="h5">Organic Tomato</a>
+                    <div className="d-flex my-3">
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star text-primary" />
+                      <i className="fas fa-star" />
+                    </div>
+                    <h4 className="mb-3">3.12 $</h4>
+                    <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary" /> Add to cart</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-6 col-xl-3">
+              <div className="text-center">
+                <img src="img/fruite-item-1.jpg" className="img-fluid rounded" alt="" />
+                <div className="py-4">
+                  <a href="#" className="h5">Organic Tomato</a>
+                  <div className="d-flex my-3 justify-content-center">
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star" />
+                  </div>
+                  <h4 className="mb-3">3.12 $</h4>
+                  <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary" /> Add to cart</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-6 col-xl-3">
+              <div className="text-center">
+                <img src="img/fruite-item-2.jpg" className="img-fluid rounded" alt="" />
+                <div className="py-4">
+                  <a href="#" className="h5">Organic Tomato</a>
+                  <div className="d-flex my-3 justify-content-center">
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star" />
+                  </div>
+                  <h4 className="mb-3">3.12 $</h4>
+                  <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary" /> Add to cart</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-6 col-xl-3">
+              <div className="text-center">
+                <img src="img/fruite-item-3.jpg" className="img-fluid rounded" alt="" />
+                <div className="py-4">
+                  <a href="#" className="h5">Organic Tomato</a>
+                  <div className="d-flex my-3 justify-content-center">
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star" />
+                  </div>
+                  <h4 className="mb-3">3.12 $</h4>
+                  <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary" /> Add to cart</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-6 col-xl-3">
+              <div className="text-center">
+                <img src="img/fruite-item-4.jpg" className="img-fluid rounded" alt="" />
+                <div className="py-2">
+                  <a href="#" className="h5">Organic Tomato</a>
+                  <div className="d-flex my-3 justify-content-center">
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star text-primary" />
+                    <i className="fas fa-star" />
+                  </div>
+                  <h4 className="mb-3">3.12 $</h4>
+                  <a href="#" className="btn border border-secondary rounded-pill px-3 text-primary"><i className="fa fa-shopping-bag me-2 text-primary" /> Add to cart</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Bestsaler Product End */}
         <Footer/>
         {/* Featurs End */}
             </div>
