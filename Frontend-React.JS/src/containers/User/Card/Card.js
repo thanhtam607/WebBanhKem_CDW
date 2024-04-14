9c224a397d82a9d0f087649b38d7d47f472dd802
Tin nguyen <20130436@st.hcmuaf.edu.vn>
2024-04-14 14:26:18 +0700

    update card

--- Frontend-React.JS/src/containers/User/Card/Card.js
+++ Frontend-React.JS/src/containers/User/Card/Card.js
@@ -36,6 +36,7 @@ class Card extends Component {
               <table className="table">
                 <thead>
                   <tr>
+                    <th scope="col">#</th>
                     <th scope="col">Products</th>
                     <th scope="col">Tên</th>
                     <th scope="col">Giá</th>
@@ -47,12 +48,15 @@ class Card extends Component {
                 <tbody>
                   <tr>
                     <th scope="row">
-                      <div className="d-flex align-items-center">
+                    <input type="checkbox" />
+                    </th>
+                    <td>
+                     <div className="d-flex align-items-center">
                         <img src="img/vegetable-item-3.png" className="img-fluid me-5 rounded-circle" style={{width: '80px', height: '80px'}} alt="" />
                       </div>
-                    </th>
+                    </td>
                     <td>
-                      <p className="mb-0 mt-4">Big Banana</p>
+                      <p className="mb-0 mt-4 col-name">Big Banana</p>
                     </td>
                     <td>
                       <p className="mb-0 mt-4">2.99 $</p>
@@ -83,48 +87,15 @@ class Card extends Component {
                   </tr>
                   <tr>
                     <th scope="row">
-                      <div className="d-flex align-items-center">
-                        <img src="img/vegetable-item-5.jpg" className="img-fluid me-5 rounded-circle" style={{width: '80px', height: '80px'}} alt="" />
-                      </div>
+                    <input type="checkbox" />
                     </th>
                     <td>
-                      <p className="mb-0 mt-4">Potatoes</p>
-                    </td>
-                    <td>
-                      <p className="mb-0 mt-4">2.99 $</p>
-                    </td>
-                    <td>
-                      <div className="input-group quantity mt-4" style={{width: '100px'}}>
-                        <div className="input-group-btn">
-                          <button className="btn btn-sm btn-minus rounded-circle bg-light border">
-                            <i className="fa fa-minus" />
-                          </button>
-                        </div>
-                        <input type="text" className="form-control form-control-sm text-center border-0" defaultValue={1} />
-                        <div className="input-group-btn">
-                          <button className="btn btn-sm btn-plus rounded-circle bg-light border">
-                            <i className="fa fa-plus" />
-                          </button>
-                        </div>
+                     <div className="d-flex align-items-center">
+                        <img src="img/vegetable-item-3.png" className="img-fluid me-5 rounded-circle" style={{width: '80px', height: '80px'}} alt="" />
                       </div>
                     </td>
                     <td>
-                      <p className="mb-0 mt-4">2.99 $</p>
-                    </td>
-                    <td>
-                      <button className="btn btn-md rounded-circle bg-light border mt-4">
-                        <i className="fa fa-times text-danger" />
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">
-                      <div className="d-flex align-items-center">
-                        <img src="img/vegetable-item-2.jpg" className="img-fluid me-5 rounded-circle" style={{width: '80px', height: '80px'}} alt="" />
-                      </div>
-                    </th>
-                    <td>
-                      <p className="mb-0 mt-4">Awesome Brocoli</p>
+                      <p className="mb-0 mt-4 col-name">Big Banana Big Banana Big Banana Big Banana Big Banana</p>
                     </td>
                     <td>
                       <p className="mb-0 mt-4">2.99 $</p>
