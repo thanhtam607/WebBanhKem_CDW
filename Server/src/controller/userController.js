3f5eec1a87e56678ea6195dbd24e9685a7eb7eb6
ThanhTam <20130396@st.hcmuaf.edu.vn>
2024-05-13 21:09:21 +0700

    add api login

--- Server/src/controller/userController.js
+++ Server/src/controller/userController.js
@@ -1,7 +1,16 @@
-let handleLogin = (req, res)=>{
+import userService from "../Service/userService"
+let handleLogin = async (req, res) => {
     let email = req.body.email
-    let pass = req.body.password
+    let pass = req.body.pass
+    console.log(email)
+    let userData = await userService.handleLogin(email, pass)
+    return res.status(200).json({
+        errCode: userData.errCode,
+        message: userData.errMessage,
+        userData
+    })
 }
+
 module.exports={
     handleLogin: handleLogin
 }
